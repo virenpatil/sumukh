6296fa47acb3762cc8829d52c235d5bfe53f1f12
santosh <santosh.ece06@gmail.com>
2017-05-12 19:23:36 +0530

    commit. client.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 console.log("Hello World");
+
+function createPost(elemObj) {
+    var post = $("#postId").val();
+
+    var inputData = {
+        "POST": post,
+        "POST_TYPE": "TEXT",
+        "PATH_OF_POST": null
+    }
+    var url = "http://localhost:3010/post/";
+    var jqxhr = $.post(url, inputData);
+    jqxhr.done(function(data) {
+        console.log(data);
+    });
+
+    jqxhr.fail(function(err) {
+        console.log(err);
+    });
+
+
+}
